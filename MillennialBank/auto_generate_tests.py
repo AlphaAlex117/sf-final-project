f31b32909f8c44625b5f3e9885dc084c1ed57f62
Alexander Lafontaine <71290520+AlphaAlex117@users.noreply.github.com>
2024-09-04 10:38:24 -0400

    Automated commit

--- MillennialBank/auto_generate_tests.py
+++ MillennialBank/auto_generate_tests.py
@@ -174,16 +174,21 @@ def generate_apex_test_cases(test_data):
         
     return "\n".join(apex_tests)
 
-# Create test input
+# Generate Apex test inputs
 def generate_apex_test_input(test_data):
     apex_test_inputs = []
     
+    update_sample = f"""
+    new List<Object>{{'ToUpdate Account', 100, 'Salary Account', null, testUpdate@pairwise.test, null, null, null, 'Yes'}}
+    """
+    apex_test_inputs.append(update_sample)
     for data in test_data:
-        test_input = f""""
-        new List<Object>{{'ToUpdate Account', 100, 'Salary Account', calculated_interest, email, interest_rate, loan_interest_rate, loan_type, remaining_loan_amount, total_loan_amount, active}}
+        test_input = f"""
+        new List<Object>{{'{data[2]}', {data[3]}, '{data[4]}', {data[5]}, '{data[6]}', {data[8]}, {data[9]}, {data[11]}, '{data[12]}'}}
         """
         apex_test_inputs.append(test_input)
 
+    return "\n".join(apex_test_inputs)
 
 # Main function to run the script
 def main():
